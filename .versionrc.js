548b7a273e60a3a2e19c98bd51fd9646e7b6127c
Jason Fung <solidzoro@live.com>
2020-09-05 14:27:14 +0800

    chore: add release minor and major

--- .versionrc.js
+++ .versionrc.js
@@ -4,9 +4,9 @@ module.exports = {
     { type: 'fix', section: 'Bug Fixes' },
     { type: 'chore', section: 'Chore' },
     { type: 'refactor', section: 'Refactor' },
+    { type: 'perf', section: 'Performance' },
     { type: 'docs', hidden: true },
     { type: 'style', hidden: true },
-    { type: 'perf', hidden: true },
     { type: 'test', hidden: true },
   ],
 };
