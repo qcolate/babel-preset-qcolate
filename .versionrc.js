0f529f21a3f3c3fb76fb927169373e3589aab23d
Jason Fung <solidzoro@live.com>
2020-09-05 14:13:39 +0800

    chore: update veriosnrc

--- .versionrc.js
+++ .versionrc.js
@@ -2,8 +2,8 @@ module.exports = {
   types: [
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
-    { type: 'chore', hidden: false },
-    { type: 'refactor', hidden: false },
+    { type: 'chore', section: 'Chore' },
+    { type: 'refactor', section: 'Refactor' },
     { type: 'docs', hidden: true },
     { type: 'style', hidden: true },
     { type: 'perf', hidden: true },
